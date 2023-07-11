565b60dc0eff3f395c2a56c899fc15b349ca4d9b
Daniel Muelle <danimr1999@gmail.com>
2023-07-11 20:03:42 +0200

    Adding confusion matrix evaluation for SciKit

--- src/scikit/model_evaluation_scikit.py
+++ src/scikit/model_evaluation_scikit.py
@@ -1,8 +1,10 @@
 import pickle
 import numpy as np
+import matplotlib.pyplot as plt
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import multilabel_confusion_matrix, classification_report, plot_confusion_matrix
 from statistics import mean, stdev
 
 # Definition of constants
@@ -15,6 +17,7 @@ data_dict = pickle.load(open(DATASET_FILE_PATH, 'rb'))
 # Get data and labels
 data = np.asarray([np.asarray(d) for d in data_dict['data']])
 labels = np.asarray(data_dict['labels'])
+unique_labels = np.unique(labels)
 
 # Create a random forest classifier
 model = RandomForestClassifier()
@@ -31,8 +34,9 @@ for i, (train_index, test_index) in enumerate(skf.split(data, labels)):
     model.fit(x_train_fold, y_train_fold)
     stratified_accuracy.append(model.score(x_test_fold, y_test_fold))
 
-# Print the output.
-print('List of possible accuracy:', stratified_accuracy)
+# Print the results for k-fold cross-validation
+print('Stratified k-fold cross-validation')
+print('List of possible accuracies:', stratified_accuracy)
 print('Maximum Accuracy:',
       max(stratified_accuracy)*100, '%')
 print('Minimum Accuracy:',
@@ -40,3 +44,43 @@ print('Minimum Accuracy:',
 print('Overall Accuracy:',
       mean(stratified_accuracy)*100, '%')
 print('Standard Deviation:', stdev(stratified_accuracy))
+print('\n')
+
+# Train the model and evaluate accuracy for the whole dataset
+model.fit(data, labels)
+print('Accuracy of the whole dataset:', model.score(data, labels)*100, '%')
+print('\n')
+
+# Confusion matrix
+y_pred = model.predict(data)
+confusion_matrix = multilabel_confusion_matrix(labels, y_pred, labels=np.unique(labels))
+report = classification_report(labels, y_pred)
+
+# Iterate over the confusion matrixes
+for i, cm in enumerate(confusion_matrix):
+      print('Confusion matrix for label', unique_labels[i])
+
+      # Extract true negatives, false positives, false negatives and true positives
+      tn, fp, fn, tp = cm.ravel()
+
+      # Print each value
+      print('True negatives:', tn)
+      print('False positives:', fp)
+      print('False negatives:', fn)
+      print('True positives:', tp)
+      print('\n')
+
+print('\n')
+print('Classification report')
+print(report)
+print('\n')
+
+# Plot the confusion matrix
+color = 'white'
+matrix = plot_confusion_matrix(model, data, y_pred, cmap=plt.cm.Blues)
+matrix.ax_.set_title('Confusion Matrix', color=color)
+plt.xlabel('Predicted Label', color=color)
+plt.ylabel('True Label', color=color)
+plt.gcf().axes[0].tick_params(colors=color)
+plt.gcf().axes[1].tick_params(colors=color)
+plt.show()
